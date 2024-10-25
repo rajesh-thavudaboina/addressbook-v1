463af99b46e8913df8f9733d7957ad4c52d39df2
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-10-25 06:33:44 +0530

    tf files

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.73.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+#   access_key = "my-access-key"
+#   secret_key = "my-secret-key"
+}
