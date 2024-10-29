fad77bc33aa2146e1aa1a4315507ad4ae1f9f3d2
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-10-29 06:39:48 +0530

    ec2-tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ terraform {
       version = "5.73.0"
     }
   }
-  backend "s3" {
-        bucket="tf-state-bucket-v1"
-        key="terraform.tfstate"
-        region="ap-south-1"
-        dynamodb_table="aws-table"
-    }
+  # backend "s3" {
+  #       bucket="tf-state-bucket-v1"
+  #       key="terraform.tfstate"
+  #       region="ap-south-1"
+  #       dynamodb_table="aws-table"
+  #   }
 
 }
 
@@ -43,27 +43,118 @@ data "aws_ami" "myami"{
 
   owners = ["amazon"]
 }
+// vpc
+resource "aws_vpc" "ownvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags={
+    Name="own-vpc"
+  }
+}
+//subnet
+resource "aws_subnet" "ownsubnet" {
+  vpc_id     = aws_vpc.ownvpc.id
+  cidr_block = "10.0.1.0/24"
+availability_zone="ap-south-1b"
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.myami.id
-  instance_type = var.instance_type
 
   tags = {
-   // Name = "tf-${count.index}"
-     Name = "tf-${terraform.workspace}"
+    Name = "own-subnet"
   }
 }
 
+//igw
+
+resource "aws_internet_gateway" "ownigw" {
+  vpc_id = aws_vpc.ownvpc.id
+
+
+  tags = {
+    Name = "own-igw"
+  }
+}
+
+//route table
+
+resource "aws_route_table" "ownrt" {
+  vpc_id = aws_vpc.ownvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ownigw.id
+  }
+  tags={
+    Name: "own-rt"
+  }
+}
+
+# 5) associate rt
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.ownsubnet.id
+  route_table_id = aws_route_table.ownrt.id
+}
+
+resource "aws_security_group" "mywebsecurity" {
+  name        = "ownsecurityrules"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.ownvpc.id
+ 
+   ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+     }
+  ingress {
+    description      = "HTTP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+     }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "own-sg"
+  }
+}
+
+
+resource "aws_instance" "webserver" {
+  ami           = data.aws_ami.myami.id
+  instance_type = var.instance_type
+   
+   associate_public_ip_address =true
+   subnet_id=aws_subnet.ownsubnet.id
+   vpc_security_group_ids = [aws_security_group.mywebsecurity.id]
+   key_name="aws"
+   user_data=file("server-script.sh")
+   
+  tags = {
+    Name = "mywebserver"
+  }
+}
+
+
 output "ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.webserver.public_ip
 }
 
 output "nwinterface" {
-  value = aws_instance.web.primary_network_interface_id
+  value = aws_instance.webserver.primary_network_interface_id
   
 }
 
 output "ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.webserver.ami
   
 }
