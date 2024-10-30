6ae2d7fd7f16552280beeb327d220d2be7c3faef
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-10-30 06:41:14 +0530

    tf code

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "webserver" {
    subnet_id=var.subnet_id
    vpc_security_group_ids = [aws_security_group.mywebsecurity.id]
    key_name="aws"
-   user_data=file("server-script.sh")
+   //user_data=file("server-script.sh")
    
   tags = {
     Name = "${var.env}-mywebserver"
