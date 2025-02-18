8b9ec6c99c67a697a1a46b31b0c7ad55ba22d285
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2025-02-18 08:52:08 +0530

    slave2

--- server-script.sh
+++ server-script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-#sudo yum install java-1.8.0-openjdk-devel -y
-sudo yum install java -y
+sudo yum install java-1.8.0-openjdk-devel -y
+#sudo yum install java -y
 sudo yum install git -y
 sudo yum install maven -y
 
